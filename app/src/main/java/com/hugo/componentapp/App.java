b4711544c1d7b2b48cba2bb721f636e9915e53a5
Hugo <297947829@qq.com>
2019-01-08 09:19:02 +0800

    .....

--- app/src/main/java/com/hugo/componentapp/App.java
+++ app/src/main/java/com/hugo/componentapp/App.java
@@ -3,7 +3,9 @@ package com.hugo.componentapp;
 import android.app.Application;
 import android.content.Context;
 
-import com.hugo.base.BaseApp;
+import com.hugo.base.config.AppConfig;
+import com.hugo.base.base.BaseApp;
+import com.hugo.base.config.ModuleInit;
 
 import androidx.multidex.MultiDex;
 
@@ -19,15 +21,19 @@ public class App extends BaseApp {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+        initModuleApp(this);
+        initModuleData(this);
     }
 
     @Override
     public void initModuleApp(Application application) {
+        ModuleInit.Companion.getInstance().initModuleApp(application);
 
     }
 
     @Override
     public void initModuleData(Application application) {
+        ModuleInit.Companion.getInstance().initModuleData(application);
 
     }
 }
